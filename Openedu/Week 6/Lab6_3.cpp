f3a8c443a4df57c4ed8eb1d4f03c136d7a432644
artamonovalex18 <artamonovalex18@gmail.com>
2019-04-15 14:16:27 +0300

    6 week 3 task

---
+++ Openedu/Week 6/Lab6_3.cpp
@@ -0,0 +1,54 @@
+﻿#include "edx-io.hpp"
+using namespace std;
+
+//Узел хранит позиции левого и правого узлов-потомков
+struct node {
+	int left, right;
+};
+
+//Возвращает максимальное из двух чисел
+long max(long a, long b) {
+	if (a > b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+//Находит глубину i-того элемента дерева tree
+long depth(node* tree, long i) {
+	long d = 0;
+	//Если предок существует, то находим его глубину
+	if (tree[i].left != 0) {
+		d = max(depth(tree, tree[i].left), d);
+	}
+	if (tree[i].right != 0) {
+		d = max(depth(tree, tree[i].right), d);
+	}
+	//Возвращаем максимальную глубину предков + 1
+	return ++d;
+}
+
+int main() {
+	long N, K;
+
+	io >> N;
+	if (N != 0) {
+		//Создаём массив узлов
+		node* tree = new node[N + 1];
+
+		for (long i = 1; i < N + 1; i++)
+		{
+			io >> K >> tree[i].left >> tree[i].right;
+		}
+		//Находим глубину 1 узла (корня)
+		io << depth(tree, 1);
+	}
+	else
+	{
+		io << 0;
+	}
+
+	return 0;
+}
