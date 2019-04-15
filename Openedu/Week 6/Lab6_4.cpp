ad92ac9dd0308c58e765569d795651a7e978344e
artamonovalex18 <artamonovalex18@gmail.com>
2019-04-15 23:33:30 +0300

    6 week 4 task added comments

--- Openedu/Week 6/Lab6_4.cpp
+++ Openedu/Week 6/Lab6_4.cpp
@@ -1,10 +1,11 @@
-#include "edx-io.hpp"
+﻿#include "edx-io.hpp"
 using namespace std;
 
 struct node {
 	long key, left, right;
 };
 
+//Определяет количество узлов в поддереве узла
 long depth(node* tree, long i) {
 	long d = 0;
 
@@ -18,6 +19,7 @@ long depth(node* tree, long i) {
 	return ++d;
 }
 
+//Находит узел по значению
 long find_node(node* tree, long del_value) {
 	long i = 1;
 
@@ -43,16 +45,20 @@ long find_node(node* tree, long del_value) {
 	return i;
 }
 
+//Удаляет узел и его поддерево по значению и возвращает количество элементов в дереве
 long delete_node(node* tree, long del_value, long size, long* parents) {
+	//Находим узел, который необходимо удалить
 	long pos = find_node(tree, del_value);
 	if (pos == 0) {
 		return size;
 	}
 	else {
 		long parent_pos = parents[pos];
+		//Если узел является левым потомком, обнуляем левого потомка родителя
 		if (tree[parent_pos].left == pos) {
 			tree[parent_pos].left = 0;
 		}
+		//Если нет, то правого вотомка
 		else {
 			tree[parent_pos].right = 0;
 		}
@@ -72,6 +78,7 @@ int main() {
 	for (long i = 1; i < N + 1; i++)
 	{
 		io >> tree[i].key >> tree[i].left >> tree[i].right;
+		//При наличии левого и правого потомков, заносим данные об их родителе в массив parents
 		if (tree[i].left != 0) {
 			parents[tree[i].left] = i;
 		}
@@ -87,6 +94,7 @@ int main() {
 	for (long i = 0; i < M; i++)
 	{
 		io >> del_value;
+		//Для каждого вводимого значения вызываем функцию удаления элемента
 		N = delete_node(tree, del_value, N, parents);
 		io << N << "\n";
 
