fee12ae3613e94d6e051b9403f7ed3dc0665e064
artamonovalex18 <artamonovalex18@gmail.com>
2019-04-23 07:02:27 +0300

    Week 9 task 2 Map of vectors of letters positions

--- Openedu/Week 9/Lab9_2.cpp
+++ Openedu/Week 9/Lab9_2.cpp
@@ -1,17 +1,30 @@
-#include <fstream>
+﻿#include <fstream>
 #include <string>
-using namespace std;
+#include <map>
+#include <vector>
 
-long count_words(string P, long pos) {
-	char curr_letter = P[pos];
-	long counter = 0;
-	for (long i = pos + 2; i < P.length(); i++) {
-		if (P[i] == curr_letter) {
-			counter += i - pos - 1;
-		}
+using namespace std;
+//Находит сумму дистанций - разностей позиций каждого с каждым
+//Формула: сумм(((n-1) - 2(i-1)) * pos[i - 1])
+//i изменяется так, чтобы pos[i] > pos[i+1]
+long long calculate_sum_distance(vector<long> positions) {
+	long long sum = 0;
+	long long temp;
+	//Коэффициент слагаемого (n-1) - 2(i-1)
+	long k = positions.size() - 1;
+	for (long i = positions.size() - 1; i >= 0; i--) {
+		temp = (long long)k * positions[i];
+		sum += temp;
+		k -= 2;
 	}
-	return counter;
+	//Точки не включены, поэтому необходимо из каждой разности вычесть единицу
+	for (long i = 1; i < positions.size(); i++)
+	{
+		sum -= i;
+	}
+	return sum;
 }
+
 
 int main() {
 
@@ -20,7 +33,7 @@ int main() {
 
 	string P = "";
 	string tmp;
-
+	//Слепливаем строку в одну без пробелов
 	while (!input.eof()) {
 		input >> tmp;
 		if (tmp != "") {
@@ -28,10 +41,18 @@ int main() {
 		}
 		tmp = "";
 	}
+	//Ассоциативный массив списков, отсортированный по алфавиту
+	map<char, vector<long>> letters;
+	//Позицию каждой буквы в строке добавляем в соответствующий список
+	for (long i = 0; i < P.length(); i++) {
+		letters[P[i]].push_back(i);
+	}
 	long long counter = 0;
-
-	for (long i = 0; i < P.length() - 2; i++) {
-		counter += count_words(P, i);
+	//Для каждого списка с количесвом элементов > 1 находим сумму разностей его элементов
+	for (char i = 'a'; i <= 'z'; i++) {
+		if (letters.count(i) && letters[i].size() > 1) {
+			counter += calculate_sum_distance(letters[i]);
+		}
 	}
 
 	output << counter;
